15f0dcd746e8999a2dbe12056aabafcec322d7bd
Juan <Juan@LENOVO-PC>
2020-03-24 16:33:04 -0300

    rename resource

--- xFNet.Entities/Resources/ValidationResource.Designer.cs
+++ xFNet.Entities/Resources/ValidationResource.Designer.cs
@@ -22,14 +22,14 @@ namespace xFNet.Entities.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Validation {
+    internal class ValidationResource {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Validation() {
+        internal ValidationResource() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace xFNet.Entities.Resources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("xFNet.Entities.Resources.Validation", typeof(Validation).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("xFNet.Entities.Resources.ValidationResource", typeof(ValidationResource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
